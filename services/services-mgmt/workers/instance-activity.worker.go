d9004d61bcb9ea472b34b031be352b7b3add13e2
Seba G <sg@hetacode.pl>
2021-02-10 22:44:53 +0100

    #3 added WorkerManager to manage workers jobs - create, delete

--- services/services-mgmt/workers/instance-activity.worker.go
+++ services/services-mgmt/workers/instance-activity.worker.go
@@ -1,6 +1,7 @@
 package smgworkers
 
 import (
+	"fmt"
 	"log"
 	"time"
 
@@ -17,41 +18,54 @@ const (
 // InstanceActivityWorker struct
 // Worker which check instances of service linked by aggregator state key in every setting period of time
 type InstanceActivityWorker struct {
-	repository  *smgeventstore.ServiceStateRepository
-	projectName string
-	serviceName string
-	period      int64 // seconds
-	createdAt   int64
-	updatedAt   int64
+	repository     *smgeventstore.ServiceStateRepository
+	projectName    string
+	serviceName    string
+	period         int64 // seconds
+	createdAt      int64
+	updatedAt      int64
+	isRunning      bool
+	disableChannel chan<- string
 }
 
 // NewInstanceActivityWorker create new instance
-func NewInstanceActivityWorker(projectName, serviceName string, period int64) *InstanceActivityWorker {
+func NewInstanceActivityWorker(repository *smgeventstore.ServiceStateRepository, projectName, serviceName string, period int64, disableChannel chan string) *InstanceActivityWorker {
 	worker := &InstanceActivityWorker{
-		projectName: projectName,
-		serviceName: serviceName,
-		period:      period,
-		createdAt:   time.Now().Unix(),
+		repository:     repository,
+		projectName:    projectName,
+		serviceName:    serviceName,
+		period:         period,
+		createdAt:      time.Now().Unix(),
+		disableChannel: disableChannel,
 	}
 	worker.updatedAt = worker.createdAt
 
 	return worker
 }
 
-// Start worker
+// Start worker job
 func (w *InstanceActivityWorker) Start() {
 	for {
+		if !w.isRunning {
+			return
+		}
+
+		time.Sleep(time.Second)
 		if time.Now().Unix()-w.updatedAt < w.period {
 			continue
 		}
+		w.updatedAt = time.Now().Unix()
 
 		aggr := w.repository.GetAggregator(w.projectName, w.serviceName)
 		if aggr == nil {
 			log.Printf("InstanceActivityWorker | cannot find an aggregator for %s service for %s project", w.serviceName, w.projectName)
-			// TODO: disable worker
-			return
+
+			w.disableChannel <- fmt.Sprintf("%s-%s", w.projectName, w.serviceName)
+			continue
 		}
 
+		instancesCount := len(aggr.State.Instances)
+		inActiveInstancesCount := 0
 		for _, instance := range aggr.State.Instances {
 			switch instance.State {
 			case smgeventstore.Active:
@@ -63,12 +77,24 @@ func (w *InstanceActivityWorker) Start() {
 					aggr.SetInstanceAsInactive(instance.Name)
 				}
 			case smgeventstore.InActive:
-				// TODO: disable worker?
+				inActiveInstancesCount++
 			}
 		}
+
+		// No active instances - worker should be stopped
+		if instancesCount == inActiveInstancesCount {
+			w.disableChannel <- fmt.Sprintf("%s-%s", w.projectName, w.serviceName)
+			continue
+		}
+
 		events := aggr.GetPendingEvents()
 		if err := w.repository.SaveEvents(w.projectName, w.serviceName, events); err != nil {
 			log.Printf("InstanceActivityWorker | cannot save events for %s service for %s project", w.serviceName, w.projectName)
 		}
 	}
+}
+
+// Stop worker job
+func (w *InstanceActivityWorker) Stop() {
+	w.isRunning = false
 }
