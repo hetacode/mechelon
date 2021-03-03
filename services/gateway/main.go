bbf5d925db01eefec017d4093ca9c327955186f1
Seba G <sg@hetacode.pl>
2021-03-03 18:50:41 +0100

    - prepare empty skeleton for http handlers with configured cors

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
-	"os"
+	"net/http"
+	"time"
 
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -11,8 +14,40 @@ func main() {
 	godotenv.Load("../../.env.dev")
 
 	log.Println("api gateway svc is starting")
-	waitCh := make(<-chan os.Signal)
+
+	router := mux.NewRouter()
+	clientsRouter := router.PathPrefix("/clients").Subrouter()
+	frontendRouter := router.PathPrefix("/api").Subrouter()
+
+	configureClientHandlers(clientsRouter)
+	configureFrontendHandlers(frontendRouter)
+
+	corsRouter := useCORS(router)
+	srv := &http.Server{
+		Handler:      corsRouter,
+		Addr:         "0.0.0.0:4000", // TODO: move PORT to the env file
+		ReadTimeout:  time.Second * 15,
+		WriteTimeout: time.Second * 5,
+	}
 
 	log.Println("api gateway svc is running")
-	<-waitCh
+	log.Fatal(srv.ListenAndServe())
+}
+
+func configureClientHandlers(h http.Handler) {
+
+}
+
+func configureFrontendHandlers(h http.Handler) {
+
+}
+
+func useCORS(handler http.Handler) http.Handler {
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"0.0.0.0"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	cred := handlers.AllowCredentials()
+	corsHandler := handlers.CORS(headersOk, originsOk, methodsOk, cred)(handler)
+
+	return corsHandler
 }
